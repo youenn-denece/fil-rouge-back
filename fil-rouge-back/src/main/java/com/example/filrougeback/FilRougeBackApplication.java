058bb598905f584c6a8687829aa13b496bd13e4c
lucierou <lucie.rouault@live.fr>
2023-01-13 09:59:26 +0100

    first commit

---
+++ fil-rouge-back/src/main/java/com/example/filrougeback/FilRougeBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.filrougeback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilRougeBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilRougeBackApplication.class, args);
+	}
+
+}
---
+++ fil-rouge-back/src/test/java/com/example/filrougeback/FilRougeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.filrougeback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilRougeBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
