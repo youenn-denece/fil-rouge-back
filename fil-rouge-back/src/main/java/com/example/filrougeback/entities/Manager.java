6a27eed78fefcca9e259fb54db2004fd7c22dece
Alexandra MASI <masialexandra@gmail.com>
2023-01-16 11:02:44 +0100

    connexion base de données

--- fil-rouge-back/src/main/java/com/example/filrougeback/entities/Manager.java
+++ fil-rouge-back/src/main/java/com/example/filrougeback/entities/Manager.java
@@ -2,7 +2,9 @@ package com.example.filrougeback.entities;
 
 import jakarta.persistence.Entity;
 
-@Entity
+
+
+
 public class Manager {
 
 
--- fil-rouge-back/src/main/java/com/example/filrougeback/entities/User.java
+++ fil-rouge-back/src/main/java/com/example/filrougeback/entities/User.java
@@ -1,4 +1,4 @@
-package com.example.filrougeback.entities;
+/*package com.example.filrougeback.entities;
 
 import jakarta.persistence.*;
 
@@ -115,5 +115,48 @@ public class User {
                 ", email='" + email + '\'' +
                 ", password='" + password + '\'' +
                 '}';
+    }
+}*/
+
+
+package com.example.filrougeback.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class User {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+
+    private String name;
+
+    private String email;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
